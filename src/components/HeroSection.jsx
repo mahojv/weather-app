e9b2f314d5a4d3283e23f447f411e6a1a9d88497
mahojv <moisesmjavier@outlook.com>
2025-05-04 00:14:27 -0600

    style: Improve layout and styling of HeroSection component for better readability

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -33,27 +33,27 @@ export default function HeroSection({ response, loading, units, setSearch, searc
 
       {response !== null &&
         <>
-          <div className='flex items-center justify-around h-[64px] w-full  mt-6'>
+          <div className=' flex items-center justify-around h-[64px] w-full  mt-6'>
 
             <button onClick={() => setToggle(true)} className='bg-[#6e707a] h-[40px] w-[176px] text-[16px] rounded-lg cursor-pointer '>Search for places</button>
             <button onClick={geoFindAPI} className='rounded-full bg-[#4b4d61] w-[40px] h-[40px] flex justify-center cursor-pointer '><img className='w-[25px] ' src="location.svg" alt="" /></button>
           </div>
-          <div className='relative  w-full  h-[216px]  flex justify-center items-center'>
+          <div className=' relative  w-full  h-[216px]  flex justify-center items-center'>
             <figure>
               <img className='absolute top-0 left-0  h-full opacity-10 w-[400px]' src="others/Cloud-background.png" alt="" />
             </figure>
             <figure className=' h-[80%] z-auto'><img className='object-cover h-full ' src={`./weather/${weatherImg}.png`} alt="" /></figure>
           </div>
           <p className=' text-[60px] lg:text-[80px] text-white my-[20px]'>{response.main.temp}  {units === "metric" ? "°C" : "°F"}</p>
-          <p className='text-[30px] font-bold  ' >{response.weather[0].description}</p>
-          <span className='flex w-[150px] justify-between my-[10px]' >
-            <p>Today . {dateTime}</p>
+          <p className=' text-[40px] font-bold w-full text-center  ' >{response.weather[0].description}</p>
+          <span className='flex w-full justify-center   my-[10px] text-lg' >
+            <p className='' >Today ,    {dateTime}</p>
 
           </span>
 
-          <span className='flex w-[180px]  justify-center gap-2 my-[20px] items-center'>
+          <span className='  flex w-full  text-l justify-center gap-2 my-[20px] items-center'>
             <figure className=''><img src="location_on.svg" alt="" /></figure>
-            <p> {response.name}, {response.sys.country} </p>
+            <p className='' > {response.name}, {response.sys.country} </p>
 
           </span>
 
